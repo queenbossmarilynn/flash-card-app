7700c527fc375d9bed1f117a0116d46a3a2d3009
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2022-11-07 22:21:04 -0800

    In HTML added a onload so every time it loads it gives a new word/term. Both check and next button work. This is better for smaller projects, and not huge list/database.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const term = document.querySelector('.term')
+const term = document.querySelector('.term');
+const checkButton = document.querySelector('.check');
+const nextButton = document.querySelector('.next');
+const definition = document.querySelector('.definition');
+
+// Terms and Definitions List (DICTIONARY)
+words = {
+    Cosmetology: "The art and science of beautifying and improving the skin, nails, hair, and includes the study of cosmetics and their application.",
+    Ethics: "The conscious act of planning your life, instead of just letting things happen.",
+    "Game Plan": "The identification of long-term and short-term goals that helps you decide what you want out of life.",
+}
+
+//Grab the Term/Definition
+data = Object.entries(words)
+
+//Get a Random Term/Definition Function
+function getRandomWord() {
+    randomTerm = data[Math.floor(Math.random() * data.length)]
+    // console.log(randomTerm);
+    term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
+    definition.innerHTML = `<h3>${randomTerm[1]}</h3>`;
+}
+
+checkButton.addEventListener('click', function(){
+    // Check the Check Btn Works
+    // console.log("You Clicked the Check Button")
+    definition.style.display = 'block';
+})
+
+nextButton.addEventListener('click', function(){
+    // Check the Next Btn Works
+    // console.log("You Clicked the Next Button")
+    getRandomWord();
+})
+
